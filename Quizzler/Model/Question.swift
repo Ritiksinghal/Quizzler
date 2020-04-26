9b776799c1d19dfd9166991be64e80475483cfa6
Ritik Singhal <singhalritik00@gmail.com>
2020-04-27 04:35:34 +0530

    Add files via upload

---
+++ Quizzler/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Ritik on 12/12/2019.
+//  Copyright © 2019 Ritik. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
